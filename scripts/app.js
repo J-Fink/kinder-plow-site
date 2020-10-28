12d8b94931b4102214e679a6db4eca05a2f4447b
J-Fink <57537040+J-Fink@users.noreply.github.com>
2020-10-27 22:43:13 -0500

    change functions for improved menu sliding animation, some refactoring

--- scripts/app.js
+++ scripts/app.js
@@ -3,19 +3,19 @@ let bar = document.querySelector('.bar-container');
 let bar1 = document.querySelector('.bar-1');
 let bar2 = document.querySelector('.bar-2');
 let bar3 = document.querySelector('.bar-3');
-let learnMoreLink = document.querySelector('.learn-more-link')
-let educationLink = document.querySelector('.education-link')
-let technologyLink = document.querySelector('.technology-link')
-let button = document.getElementById('do-it');
-let dropdownMenu = document.querySelector('.dropdown-menu');
-let dropdownContent = document.querySelectorAll('.dropdown-content');
-bar.addEventListener('click', showMenu);
-learnMoreLink.addEventListener('click', showAbout);
-educationLink.addEventListener('click', showEducation);
-technologyLink.addEventListener('click', showTechnology);
+let historyLink = document.querySelector('.history-link')
+let bioLink = document.querySelector('.bio-link')
+let faqsLink = document.querySelector('.faqs-link')
+let purchaseLink = document.querySelector('.purchase-link')
+
+
+bar.addEventListener('click', openNav);
+historyLink.addEventListener('click', showHistory);
+bioLink.addEventListener('click', showBio);
+faqsLink.addEventListener('click', showFaqs);
+purchaseLink.addEventListener('click', showStore);
 
 document.addEventListener('scroll', scrollTest);
-bar.addEventListener('click', showMenu);
 function scrollTest(){
     if (scrollY > 0) {
         // alert('it\'s working!');
@@ -25,52 +25,73 @@ function scrollTest(){
     }
 }
 
-let x = window.matchMedia('(max-width: 500px)');
+function openNav() {
+    document.getElementById('mobileNav').style.width = '100%';
+    bar.classList.add('change');
+    bar.classList.add('closeBtn');
+    bar.classList.remove('openBtn');
+    bar.removeEventListener('click', openNav);
+    bar.addEventListener('click', closeNav);
+}
+function closeNav() {
+    document.getElementById('mobileNav').style.width = '0%';
+    bar.classList.remove('change');
+    bar.classList.add('openBtn');
+    bar.classList.remove('closeBtn');
+    bar.removeEventListener('click', closeNav);
+    bar.addEventListener('click', openNav);
+}
+// new functionality for open close menue full screen
 
-function removeHiddenClass() {
-    dropdownMenu.classList.remove('hidden');
-}
-function addHiddenClass() {
-    dropdownMenu.classList.add('hidden');
-}
+// document.querySelector('.openBtn').addEventListener('click', openNav);
+bar.addEventListener('click', openNav);
+
+// let x = window.matchMedia('(max-width: 500px)');
+
+// function removeHiddenClass() {
+//     dropdownMenu.classList.remove('hidden');
+// }
+// function addHiddenClass() {
+//     dropdownMenu.classList.add('hidden');
+// }
 
 //hamburger bar event listeners
 
 //
-function showMenu(){
-    bar.classList.add('change');
-    dropdownMenu.classList.add('expand');
-    for (let i = 0; i < dropdownContent.length; i++) {
-    dropdownContent[i].classList.add('animated');
+// function showMenu(){
+//     bar.classList.add('change');
+//     dropdownMenu.classList.add('expand');
+//     for (let i = 0; i < dropdownContent.length; i++) {
+//     dropdownContent[i].classList.add('animated');
+//     }
+//     bar.removeEventListener('click', showMenu);
+//     bar.addEventListener('click', hideMenu);
+//     }
+function showHistory(){
+    closeNav();
     }
-    bar.removeEventListener('click', showMenu);
-    bar.addEventListener('click', hideMenu);
+function showBio(){
+    closeNav();
     }
-function showTechnology(){
-    hideMenu();
+function showFaqs(){
+    closeNav();
     }
-    function showEducation(){
-    hideMenu();
-    }
-    function showAbout(){
-    hideMenu();
+function showStore(){
+    closeNav();
     }
 
-function hideMenu() {
-    bar.classList.remove('change');
-    dropdownMenu.classList.remove('expand');
-    for (let i = 0; i < dropdownContent.length; i++) {
-    dropdownContent[i].classList.remove('animated');
-    }
-    bar.removeEventListener('click', hideMenu);
-    bar.addEventListener('click', showMenu);
-    }
-function myFunction(x) {
-    if (x.matches) {
-        addHiddenClass();
-        console.log('hidden class added');
-    } else {
-        removeHiddenClass();
-        console.log('hidden class removed');
-    }}
-myFunction(x);
+// function hideMenu() {
+//     bar.classList.remove('change');
+//     dropdownMenu.classList.remove('expand');
+//     bar.removeEventListener('click', closeNav);
+//     bar.addEventListener('click', openNav);
+//     }
+// function myFunction(x) {
+//     if (x.matches) {
+//         addHiddenClass();
+//         console.log('hidden class added');
+//     } else {
+//         removeHiddenClass();
+//         console.log('hidden class removed');
+//     }}
+// myFunction(x);
